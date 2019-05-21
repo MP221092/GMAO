0dc2f6ea241b7bcf6340d48c060bb53766970b05
guibresz <guibresz02@gmail.com>
2019-05-21 15:08:30 +0200

    test avec javascript, je vais tenter autre chose

--- php/session_start.php
+++ php/session_start.php
@@ -1,11 +1,17 @@
 <?php
 session_start();
+try{
+	$_SESSION['mdp'] = $_POST["mdp"];
+}
+catch(Exception $e){
+
+}
 
 
 
 if($_SESSION['mdp'] == '1234') //si le mot de passe existe déjà
 {
-	?><meta http-equiv="refresh" content="0; URL=ajout_technicien_formulaire.php"><?php //pas besoin de se connecter ca va directement sur la page du technicien
+	?><meta http-equiv="refresh" content="0; URL=../html/service.html"><?php //pas besoin de se connecter ca va directement sur la page du technicien
 }
 
 
@@ -17,16 +23,16 @@ else{
     	var mot_de_passe = document.getElementById("mdp").value; //récupère la valeur rentrée dans le formulaire html
     	
     	if(mot_de_passe == "1234"){ //test du mot de passe
-    		alert("bon mot de passe"); //popup avec message
+    		 
     	}
     	else{
-    		alert("mauvais mot de passe");
+    		alert("mauvais mot de passe");//popup avec message
     	}
     	
 	}
 	</script>
 
-	<form>
+	<form action="session_start.php" method="post">
 		<input type="text" value="mot de passe" id="mdp" onclick="javascript:this.value=''">
     	<input type="submit" name="envoyer" value="envoyer" onclick="afficher();">
 	</form>
