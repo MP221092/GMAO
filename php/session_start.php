905867cc6a9639610742a6a20ee8d2609c399f53
guibresz <guibresz02@gmail.com>
2019-05-21 14:32:04 +0200

    documentation

--- php/session_start.php
+++ php/session_start.php
@@ -2,6 +2,7 @@
 session_start();
 
 
+
 if($_SESSION['mdp'] == '1234') //si le mot de passe existe déjà
 {
 	?><meta http-equiv="refresh" content="0; URL=ajout_technicien_formulaire.php"><?php //pas besoin de se connecter ca va directement sur la page du technicien
@@ -11,12 +12,12 @@ if($_SESSION['mdp'] == '1234') //si le mot de passe existe déjà
 else{
 	?>
 
-	<script type="text/javascript">
-	function afficher() {
-    	var mot_de_passe = document.getElementById("mdp").value;
+	<script type="text/javascript"> //utilisation de javascript
+	function afficher() { //fonction affichage si mauvais mot de passe
+    	var mot_de_passe = document.getElementById("mdp").value; //récupère la valeur rentrée dans le formulaire html
     	
-    	if(mot_de_passe == "1234"){
-    		alert("bon mot de passe");
+    	if(mot_de_passe == "1234"){ //test du mot de passe
+    		alert("bon mot de passe"); //popup avec message
     	}
     	else{
     		alert("mauvais mot de passe");
