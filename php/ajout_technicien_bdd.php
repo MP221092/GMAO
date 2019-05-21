69cff5ace84343aba2b9f3be7c4bdbe6789bbc2b
guibresz <guibresz02@gmail.com>
2019-05-21 10:29:31 +0200

    documentation du fichier add technicien bdd

--- php/ajout_technicien_bdd.php
+++ php/ajout_technicien_bdd.php
@@ -1,16 +1,16 @@
 <?php
 
-	$bdd = new PDO("mysql;host=localhost;dbname=GMAO", "root", "");
+	$bdd = new PDO("mysql;host=localhost;dbname=GMAO", "root", ""); //connexion à la base de donnée
 
-	$req = $bdd->query("SELECT * FROM technicien");
+	$req = $bdd->query("SELECT * FROM technicien"); //on récupère tout ce qu'il y a dans la table technicien pour vérif si le technicien existe déja
 
-	$existe_deja = 0;
+	$existe_deja = 0; //variable de test
 
 	while($reponse = $req->fetch() )
 	{
-		if(  strtoupper( $_POST['nom']  ) == $reponse['nom'] AND   $_POST['nom'] ==  $reponse['prenom']   )
+		if(  strtoupper( $_POST['nom']  ) == $reponse['nom'] AND   $_POST['nom'] ==  $reponse['prenom']   ) //verif si prenom et nom existe deja avec majuscul
 		{
-			$existe_deja = 1;
+			$existe_deja = 1; //si on trouve on modifie la variable de test
 		}
 	}
 
