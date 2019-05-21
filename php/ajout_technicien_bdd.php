6a96604c6b5a8df3eaf58a420cd0a6e95521302e
guibresz <guibresz02@gmail.com>
2019-05-21 11:02:20 +0200

    rajout de documentation, verif et ajout dans bdd avec nom en majuscule et prenom en minuscule

--- php/ajout_technicien_bdd.php
+++ php/ajout_technicien_bdd.php
@@ -1,6 +1,11 @@
 <?php
 
-	$bdd = new PDO("mysql;host=localhost;dbname=GMAO", "root", ""); //connexion à la base de donnée
+	try{
+		$bdd = new PDO("mysql:host=localhost;dbname=gmao", "root", "", array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION)); //connexion à la base de donnée
+	}
+	catch(Exception $e){
+		die('Erreur : ' .$e->getMessage());
+	}
 
 	$req = $bdd->query("SELECT * FROM technicien"); //on récupère tout ce qu'il y a dans la table technicien pour vérif si le technicien existe déja
 
@@ -8,10 +13,33 @@
 
 	while($reponse = $req->fetch() )
 	{
-		if(  strtoupper( $_POST['nom']  ) == $reponse['nom'] AND   $_POST['nom'] ==  $reponse['prenom']   ) //verif si prenom et nom existe deja avec majuscul
+		if(  strtoupper( $_POST['nom']  ) == $reponse['nom'] AND   strtolower($_POST['nom']) ==  $reponse['prenom']   ) //verif si prenom et nom existe deja avec majuscule et si prenom existe déja en minuscule (convention quand je vais les rentrer dans la BDD)
 		{
 			$existe_deja = 1; //si on trouve on modifie la variable de test
 		}
+	}
+
+	if($existe_deja == 1) //si la variable a été modif alors le nom est déjà rentré donc mauvais formulaire
+	{
+		echo '<list>Le technicien que vous avez rentré existe déjà</list>';
+
+		?><meta http-equiv="refresh" content="5; URL=ajout_technicien_formulaire.php"><?php //renvoie sur le forumaire
+	}
+
+	else
+	{
+		$prep = $bdd->prepare('INSERT INTO technicien(prenom, nom) VALUES (:prenom,:nom)');
+		
+		$prep->execute(array(
+		'prenom' => strtolower($_POST['prenom']),
+		'nom' => strtoupper($_POST['nom']) 
+		));
+
+
+		echo '<list>' .strtolower($_POST['prenom']). ' ' .strtoupper($_POST['nom']) . ' a été ajouté</list>';
+
+		?><meta http-equiv="refresh" content="5; URL=ajout_technicien_formulaire.php"><?php //renvoie sur le forumair
+
 	}
 
 ?>
