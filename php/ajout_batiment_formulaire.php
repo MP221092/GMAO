a46860cf15754d0d53cef3a22aae753b89113650
guibresz <guibresz02@gmail.com>
2019-05-22 08:48:29 +0200

    formulaire pour ajouter batiment

---
+++ php/ajout_batiment_formulaire.php
@@ -0,0 +1,12 @@
+
+
+<form action="ajout_batiment_bdd.php" method="post">
+	<div>
+	    <label >Nom du bâtiment :</label>
+	    <input type="text" name="nom" required>
+	</div>
+	<div class="button">
+        <button type="submit">Ajouter</button>
+    </div>
+
+</form>
